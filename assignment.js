f208e9758fa849d54692c15587a6dc9419c5ae80
hanif365 <m.a.hanifkhan64@gmail.com>
2021-01-22 01:37:13 +0600

    added task 1

---
+++ assignment.js
@@ -0,0 +1,11 @@
+//Task 1: A Program to convert kilometer to meter.
+
+function kilometerToMeter(kiloMeter){
+    var meter;
+    meter = kiloMeter * 1000;   // 1 kilometer = 1000 meter.
+    return meter;
+}
+
+var result = kilometerToMeter(15);
+console.log(result);
+
