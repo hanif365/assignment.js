7e997b597d54184477d9ff171bd0d6b172e31575
hanif365 <m.a.hanifkhan64@gmail.com>
2021-01-22 02:48:28 +0600

    added task 2

--- assignment.js
+++ assignment.js
@@ -1,3 +1,4 @@
+/*
 //Task 1: A Program to convert kilometer to meter.
 
 function kilometerToMeter(kiloMeter){
@@ -6,6 +7,47 @@ function kilometerToMeter(kiloMeter){
     return meter;
 }
 
-var result = kilometerToMeter(15);
-console.log(result);
+var result1 = kilometerToMeter(15);
+console.log(result1);
+*/
+
+
+
+
+/*
+
+
+// Task 2: Budget Calculator
+
+function budgetCalculator(watchCount, phoneCount, laptopCount){
+
+    // If any of the product Count values is negative then the function returns an error message.
+
+    if((watchCount < 0) || (phoneCount < 0) || (laptopCount < 0)){
+        return "Error! Product count is not considered a negative value. Please give a positive product count.";
+    }
+
+    var watchUnitPrice = 50;
+    var totalWatchPrice = watchCount * watchUnitPrice;
+
+    var phoneUnitPrice = 100;
+    var totalPhonePrice = phoneCount * phoneUnitPrice;
+
+    var laptopUnitPrice  = 500;
+    var totalLaptopPrice = laptopCount * laptopUnitPrice;
+
+    var totalBudget = totalWatchPrice + totalPhonePrice + totalLaptopPrice;
+
+    return totalBudget;
+}
+
+var result2 = budgetCalculator(10,10,10);
+console.log(result2);
+
+*/
+
+
+
+
+
 
